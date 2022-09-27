8c530b1c95022b0ed90b6b39369f7e8e75dd623b
Tafita Hery <tafitahery@gmail.com>
2022-09-27 20:16:12 +0300

    Node.js MongoDB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 const app = express();
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log('DB connexion successfull'))
+  .catch((err) => console.log(err));
 
 app.listen(8800, () => {
   console.log('Backend server is running on port 8800');
