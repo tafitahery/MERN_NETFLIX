83ee3cd9c9c85eb98798ae2b3f490ac38c55bf94
Tafita Hery <tafitahery@gmail.com>
2022-09-27 17:16:06 +0300

    React Register Page Design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import './app.scss';
-import Watch from './pages/watch/Watch';
+import Register from './pages/register/Register';
+// import Watch from './pages/watch/Watch';
 // import Home from './pages/home/Home';
 
 const App = () => {
   return (
     <div>
       {/* <Home /> */}
-      <Watch />
+      {/* <Watch /> */}
+      <Register />
     </div>
   );
 };
