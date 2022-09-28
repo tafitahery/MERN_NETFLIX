edeb73d61c39af30113258a833c45503039c9135
Tafita Hery <tafitahery@gmail.com>
2022-09-28 06:36:54 +0300

    Node.js Register

--- api/index.js
+++ api/index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+const authRoute = require('./routes/auth');
+
 const app = express();
+app.use(express.json());
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log('DB connexion successfull'))
   .catch((err) => console.log(err));
+
+app.use('/api/auth', authRoute);
 
 app.listen(8800, () => {
   console.log('Backend server is running on port 8800');
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,5 +1,22 @@
 const router = require('express').Router();
+const CryptoJS = require('crypto-js');
 
 const User = require('../models/User');
+
+//REGISTER
+router.post('/register', async (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.HmacSHA1(req.body.password, process.env.SECRET_KEY),
+  });
+
+  try {
+    const savedUser = await user.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
