7db297f2bd167555797b6bdbd212d95def23e3ee
Tafita Hery <tafitahery@gmail.com>
2022-09-29 17:58:03 +0300

    Rest API CRUD

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 const authRoute = require('./routes/auth');
+const userRoute = require('./routes/users');
 
 const app = express();
 app.use(express.json());
@@ -14,6 +15,7 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
 
 app.listen(8800, () => {
   console.log('Backend server is running on port 8800');
