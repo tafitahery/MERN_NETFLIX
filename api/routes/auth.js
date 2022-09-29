26a56847115b22d4a3c35c69feeb409875904eb2
Tafita Hery <tafitahery@gmail.com>
2022-09-29 14:36:00 +0300

    Login with JWT Authentification

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,19 +1,51 @@
 const router = require('express').Router();
-const CryptoJS = require('crypto-js');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');
 
 //REGISTER
 router.post('/register', async (req, res) => {
-  const user = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: CryptoJS.HmacSHA1(req.body.password, process.env.SECRET_KEY),
-  });
-
   try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const user = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
     const savedUser = await user.save();
     res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//LOGIN
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(401).json('Wrong password or email');
+    }
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword) {
+      return res.status(401).json('Wrong password or email');
+    }
+
+    const accessToken = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.SECRET_KEY,
+      { expiresIn: '5d' }
+    );
+
+    const { password, ...info } = user._doc;
+    res.status(200).json({ ...info, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }
