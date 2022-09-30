786e9c41fd8f3de6925ad3fb55b18832b6fa8de3
Tafita Hery <tafitahery@gmail.com>
2022-09-30 17:35:04 +0300

    Rest API Movie CRUD Operations

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
+const movieRoute = require('./routes/movies');
 
 const app = express();
 app.use(express.json());
@@ -16,6 +17,7 @@ mongoose
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
+app.use('/api/movies', movieRoute);
 
 app.listen(8800, () => {
   console.log('Backend server is running on port 8800');
